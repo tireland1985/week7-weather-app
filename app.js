25e99518683bb3f57283fdfe6af297ec007ca024
tireland1985 <ireland.t@gmail.com>
2020-03-10 12:06:06 +0000

    bug squashing 10/03/2020@12:06

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// select elements using dom querySelector
+// weather app class project
+// select elements using dom events .querySelector
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
@@ -16,28 +17,29 @@ const KELVIN = 273;
 //personal api key from openweathermap.org - if you're copying this repo, please get your own key 
 const key = "e8bc17b2db932bfbd230b3dc3e990eaf";
 
-//geolocation checking
+//geolocation checking - allow in browser & may need to disable adblocker
 if('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
-} else {
-    notificationElement.getElementsByClassName.display = "block";
+}else{
+    notificationElement.style.display = "block";
     notificationElement.innerHTML = "<p>Your browser doesn't support Geolocation</p>";
 }
 //set user position
-function setPosition(position) {
+function setPosition(position){
     let latitude = position.coords.latitude;
-    let longtitude = position.coords.longtitude;
+    let longitude = position.coords.longitude;
 
-    getWeather(latitude, longtitude);
+    getWeather(latitude, longitude);
 }
 
 //show error if service problem
-function showError(error) {
+function showError(error){
     notificationElement.style.display = "block";
-    notificationElement.innerHTML = `<p> ${error.message} </p>`
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
-function getWeather(latitude, longtitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${key}`;
+//grab data from provider API - bring in json api data via fetch below
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     fetch(api)
         .then(function(response){
@@ -45,7 +47,7 @@ function getWeather(latitude, longtitude) {
             return data;
         })
     .then(function(data){
-        weather.temperature.value = math.floor(data.main.temp - KELVIN);
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
         weather.description = data.weather[0].description;
         weather.iconId = data.weather[0].icon;
         weather.city = data.name;
@@ -55,19 +57,21 @@ function getWeather(latitude, longtitude) {
         displayWeather();
     });
 }
+// this function should show data in the UI with icons
 function displayWeather() {
     iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
+// C to F temperature conversion - necessary because the api provides the data in Kelvin
 function celsiusToFahrenheit(temperature) {
     return (temperature * 9/5) +32;
 }
 tempElement.addEventListener("click", function() {
     if(weather.temperature.value === undefined) return;
 
-    if(weather.temperature.unit =="celsius") {
+    if(weather.temperature.unit == "celsius") {
         let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
         fahrenheit = Math.floor(fahrenheit);
 
